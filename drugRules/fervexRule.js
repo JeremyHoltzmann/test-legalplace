586a0631f35bd83d988e11b69d760051ae9ad646
JeremyHoltzmann <gg1233@gmail.com>
2026-02-12 16:14:43 +0100

    Update Fervex Rule Add more tests

--- drugRules/fervexRule.js
+++ drugRules/fervexRule.js
@@ -4,8 +4,9 @@ export class FervexRule extends DrugRule {
   updateBenefitValue(drug) {
     let benefitIncrease = 1;
 
-    if (drug.expiresIn < 0) {
+    if (drug.expiresIn <= 0) {
       drug.benefit = 0;
+      return;
     }
 
     if (drug.expiresIn <= 10) benefitIncrease = benefitIncrease + 1;
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -9,6 +9,14 @@ describe("Pharmacy", () => {
 });
 
 describe("Pharmacy", () => {
+  it("should decrease the benefit by 2 and expiresIn by 1", () => {
+    expect(
+      new Pharmacy([new Drug("test", -1, 3)]).updateBenefitValue(),
+    ).toEqual([new Drug("test", -2, 1)]);
+  });
+});
+
+describe("Pharmacy", () => {
   it("should decrease the expiresIn and benefit should stay at 0", () => {
     expect(new Pharmacy([new Drug("test", 2, 0)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 0)],
@@ -29,5 +37,45 @@ describe("Pharmacy", () => {
     expect(
       new Pharmacy([new Drug("Herbal Tea", -1, 50)]).updateBenefitValue(),
     ).toEqual([new Drug("Herbal Tea", -2, 50)]);
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should keep the benefit and the expireIn at 3", () => {
+    expect(
+      new Pharmacy([new Drug("Magic Pill", 3, 3)]).updateBenefitValue(),
+    ).toEqual([new Drug("Magic Pill", 3, 3)]);
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should increase the benefit by 1 and decrease the expireIn by 1", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 20, 3)]).updateBenefitValue(),
+    ).toEqual([new Drug("Fervex", 19, 4)]);
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should increase the benefit by 2 and decrease the expireIn by 1", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 10, 3)]).updateBenefitValue(),
+    ).toEqual([new Drug("Fervex", 9, 5)]);
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should increase the benefit by 3 and decrease the expireIn by 1", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 5, 6)]).updateBenefitValue(),
+    ).toEqual([new Drug("Fervex", 4, 9)]);
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should set the benefit at 0 and decrease benefit by 1", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 0, 6)]).updateBenefitValue(),
+    ).toEqual([new Drug("Fervex", -1, 0)]);
   });
 });
